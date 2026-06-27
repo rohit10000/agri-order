a91a6dcf6c8587cdbfc0f22d5587c2138ad41834
rohitsingh <rohitsingh18997@gmail.com>
2026-06-27 21:58:50 +0530

    first commit

---
+++ src/test/java/in/agri/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package in.agri.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
